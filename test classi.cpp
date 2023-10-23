cbb8548341fa152ecf026cabcbc1b4f868db30de
semreh17 <77502988+semreh17@users.noreply.github.com>
2023-10-23 19:01:02 +0200

    Update test classi.cpp

--- test classi.cpp
+++ test classi.cpp
@@ -1,11 +1,25 @@
+#include <cstdlib>
 #include <iostream>
+#include <time.h>
+#include <vector>
 #include "noDuplication.h"
 using namespace std;
 
 
 int main() {
-    int arr[] = {0, 1, 2,3, 1,7,3};
-    int l = 7;
+    std::vector<int> arr;
+    int l;
+    srand(time(0));
+    cout << "inserire la lunghezza dell'array: ";
+    cin >> l;
+    arr.resize(l);
+    for(int i = 0; i < l; i++) {
+        arr[i] = (rand() % l-1);
+//        cout << arr[i] << "\t";
+    }
+
+    cout << endl;
+
     noDuplication obj1(arr, l);
     obj1.arrIteration();
 }

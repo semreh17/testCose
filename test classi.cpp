988b165e4c23d10766001b65f40a20d8a7c10f27
semreh17 <77502988+semreh17@users.noreply.github.com>
2023-10-24 10:00:26 +0200

    classTest.cpp

--- test classi.cpp
+++ test classi.cpp
@@ -7,18 +7,21 @@ using namespace std;
 
 
 int main() {
+    //
     std::vector<int> arr;
     int l;
     srand(time(0));
-    cout << "inserire la lunghezza dell'array: ";
+    
+    cout << "insert array's length: ";
     cin >> l;
+    //dynamic allocation of arr with the length insert by the user
     arr.resize(l);
     for(int i = 0; i < l; i++) {
-        arr[i] = (rand() % l-1);
-//        cout << arr[i] << "\t";
+        arr[i] = (rand() % l);
+        cout << arr[i] << "\t";
     }
 
-    cout << endl;
+    cout << endl << endl;
 
     noDuplication obj1(arr, l);
     obj1.arrIteration();

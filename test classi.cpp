711adf07fa6b349984cb40052ce695d241461979
semreh17 <77502988+semreh17@users.noreply.github.com>
2023-10-24 10:01:30 +0200

    Update test classi.cpp

--- test classi.cpp
+++ test classi.cpp
@@ -7,7 +7,6 @@ using namespace std;
 
 
 int main() {
-    //
     std::vector<int> arr;
     int l;
     srand(time(0));
@@ -16,7 +15,7 @@ int main() {
     cin >> l;
     //dynamic allocation of arr with the length insert by the user
     arr.resize(l);
-    for(int i = 0; i < l; i++) {
+    for (int i = 0; i < l; i++) {
         arr[i] = (rand() % l);
         cout << arr[i] << "\t";
     }
@@ -25,4 +24,5 @@ int main() {
 
     noDuplication obj1(arr, l);
     obj1.arrIteration();
+    return 0;
 }
